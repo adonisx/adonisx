d5a8995e691ba14e6189e3b46cf085e2b37fcda3
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2020-04-05 17:23:42 +0300

    All triggers have been implemented.

--- src/Controllers/XController.js
+++ src/Controllers/XController.js
@@ -24,7 +24,7 @@ class XController {
     // Executing query
     const result = await query.paginate(1, 10)
 
-    // We should trigger onBeforePagination events
+    // We should trigger onAfterPagination events
     await Trigger.fire('onAfter', modelPath, 'paginate', { result })
     
     // And this is my function response
@@ -33,24 +33,36 @@ class XController {
 
   async show ({ request, response, params }) {
     // Loading model
-    const Model = this.getModel(request)
+    const route = RouteHelper.get(request.apix.url)
+    const modelPath = `App/Models/${route.model}`
+    const Model = use(modelPath)
 
     // Fetching item
     const query = Model.query()
     this.appentParentIdCondition(request, params, query)
 
+    // We should add this condition in here because of performance.
+    query.where('id', params.id)
+
+    // We should trigger onBeforeFirstOrFail events
+    await Trigger.fire('onBefore', modelPath, 'firstOrFail', { query })
+
     const item = await this.safe(request, async () => {
-      return await query
-        .where('id', params.id)
-        .firstOrFail()
+      return await query.firstOrFail()
     })
+
+    // We should trigger onAfterFirstOrFail events
+    await Trigger.fire('onAfter', modelPath, 'firstOrFail', { item })
 
     return response.json(item)
   }
 
   async store ({ request, response, params }) {
     // Loading model
-    const Model = this.getModel(request)
+    const route = RouteHelper.get(request.apix.url)
+    const modelPath = `App/Models/${route.model}`
+    const Model = use(modelPath)
+
     // We should validate the data
     await Validation.validate(request.all(), Model.validations)
     // Preparing the data
@@ -59,21 +71,38 @@ class XController {
     if (request.apix.parent_column) {
       data[snakeCase(request.apix.parent_column)] = params[request.apix.parent_column]
     }
+
+    // We should trigger onBeforeCreate events
+    await Trigger.fire('onBefore', modelPath, 'create', { request, params, data })
+
     // Creating the item
     const item = await Model.create(data)
+
+    // We should trigger onAfterCreate events
+    await Trigger.fire('onAfter', modelPath, 'create', { request, params, data, item })
+
     // Returning response
     return response.json(item)
   }
 
   async update ({ request, response, params }) {
     // Loading model
-    const Model = this.getModel(request)
+    const route = RouteHelper.get(request.apix.url)
+    const modelPath = `App/Models/${route.model}`
+    const Model = use(modelPath)
+
     // Fetching item
-    const query = Model.query()
+    const query = Model.query().where('id', params.id)
+
     this.appentParentIdCondition(request, params, query)
-    const item = await query
-      .where('id', params.id)
-      .firstOrFail()
+
+    // We should trigger onBeforeUpdateQuery events
+    await Trigger.fire('onBefore', modelPath, 'updateQuery', { request, params, query })
+
+    const item = await query.firstOrFail()
+
+    // We should trigger onAfterUpdateQuery events
+    await Trigger.fire('onAfter', modelPath, 'updateQuery', { request, params, item })
 
     // Preparing the data
     const data = request.only(Model.fillable)
@@ -88,7 +117,13 @@ class XController {
     // We should validate the data
     await Validation.validate(item.toJSON(), Model.validations)
 
+    // We should trigger onBeforeUpdate events
+    await Trigger.fire('onBefore', modelPath, 'update', { request, params, item })
+
     await item.save()
+
+    // We should trigger onAfterUpdate events
+    await Trigger.fire('onAfter', modelPath, 'update', { request, params, item })
 
     // Returning response
     return response.json(item)
@@ -96,7 +131,9 @@ class XController {
 
   async destroy ({ request, response, params }) {
     // Loading model
-    const Model = this.getModel(request)
+    const route = RouteHelper.get(request.apix.url)
+    const modelPath = `App/Models/${route.model}`
+    const Model = use(modelPath)
 
     // Fetching item
     const query = Model
@@ -106,8 +143,17 @@ class XController {
     // Appending parent id condition
     this.appentParentIdCondition(request, params, query)
 
+    // We should trigger onBeforeDelete events
+    await Trigger.fire('onBefore', modelPath, 'delete', { request, params, query })
+
+    const item = (await query.firstOrFail()).toJSON()
+
     // Deleting the item
     await query.delete()
+
+    // We should trigger onAfterDelete events
+    await Trigger.fire('onAfter', modelPath, 'delete', { item })
+
     return response.ok()
   }
 
@@ -140,11 +186,6 @@ class XController {
     if (request.apix.parent_column) {
       query.where(snakeCase(request.apix.parent_column), params[request.apix.parent_column])
     }
-  }
-
-  getModel (request) {
-    const route = RouteHelper.get(request.apix.url)
-    return use(`App/Models/${route.model}`)
   }
 }
 
