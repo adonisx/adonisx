c87aec12fc9bb90ac85196ed82df450d1a56a065
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2020-04-08 21:06:35 +0300

    RouteHelper tests have been added.

---
+++ tests/src/Helpers/RouteHelper.test.js
@@ -0,0 +1,15 @@
+const RouteHelper = require(`${src}/Helpers/RouteHelper`)
+
+test('Route helper should be able to hold models for url definitions', () => {
+  const helper = new RouteHelper()
+  
+  helper.set('api/users', 'User')
+  expect(helper.get('api/users')).toBe('User')
+})
+
+test('Route helper should be able to hold middlewares for url id keys', () => {
+  const helper = new RouteHelper()
+  
+  helper.setMiddleware('userId', 'User')
+  expect(helper.getMiddlewareModel('userId')).toBe('User')
+})
